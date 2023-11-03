6d2b26e02f42c88bae9e10e229c770f18764d1ee
Anurag Singh <anuragsingh121124@gmail.com>
2023-11-03 22:40:03 +0530

    microservics

---
+++ hotel-microservice/src/main/java/com/hotelmicroservice/Entites/Hotel.java
@@ -0,0 +1,26 @@
+package com.hotelmicroservice.Entites;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "hotels")
+public class Hotel {
+
+    @Id
+    private String id;
+    private String name;
+    private String location;
+    private String about;
+    
+    
+}
---
+++ hotelrating/src/test/java/com/hotelrating/hotelrating/HotelratingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelrating.hotelrating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelratingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
